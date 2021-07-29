6e245613ced0ee6c0ff9ff2b64da3b795aeb65a6
SawyerOG <oglesbysaw@gmail.com>
2021-07-29 16:38:48 -0600

    move url to env

--- src/Config/URL.tsx
+++ src/Config/URL.tsx
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
-export const socket = io('http://localhost:5000/');
+export const backendURL = process.env.REACT_APP_BACKEND_URL as string;
+
+export const socket = io(backendURL);
 
 socket.on('connect', () => console.log('we is connected'));
-
-export const backendURL = process.env.REACT_APP_BACKEND_URL as string;
